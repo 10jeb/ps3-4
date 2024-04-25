6e57eaf6986708897db963e200332987f436a72b
10jeb <57235747+10jeb@users.noreply.github.com>
2024-04-25 13:18:35 -0500

    Update webapp2.js

--- webapp2.js
+++ webapp2.js
@@ -11,25 +11,25 @@
 //     const collection = db.collection('places');
 
     var http = require('http');
-    //var url = require('url');
+    var url = require('url');
     var port = process.env.PORT || 3000;
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        //var path = url.parse(req.url).pathname;
-        //var querystr = url.parse(req.url).query;
-        // if (req.url == "/home"){
-        //     form = "<form action='/process' method='get'>" +
-        //             "Place or Zip <input type='text' name='data' id='data'><br>" +
-        //             "<input type='submit'>" +
-        //             "</form>";
-        //     res.write(form);
-        // } else if (path == "/process"){
-        //     if (/^\d/.test(url.parse(req.url, true).query.data)) {
-        //         res.write("You entered a zipcode: " + url.parse(req.url, true).query.data);
-        //     } else {
-        //         res.write("You entered a place: " + url.parse(req.url, true).query.data);
-        //     }
-        // }
+        var path = url.parse(req.url).pathname;
+        var querystr = url.parse(req.url).query;
+        if (req.url == "/"){
+            form = "<form action='/process' method='get'>" +
+                    "Place or Zip <input type='text' name='data' id='data'><br>" +
+                    "<input type='submit'>" +
+                    "</form>";
+            res.write(form);
+        } else if (path == "/process"){
+            if (/^\d/.test(url.parse(req.url, true).query.data)) {
+                res.write("You entered a zipcode: " + url.parse(req.url, true).query.data);
+            } else {
+                res.write("You entered a place: " + url.parse(req.url, true).query.data);
+            }
+        }
         res.write("helloooooo");
         res.end();
     }).listen(port);
