bdd2f3a799bf75c396b0caf077b7ca272940b9f5
10jeb <57235747+10jeb@users.noreply.github.com>
2024-04-25 13:42:10 -0500

    Update webapp2.js

--- webapp2.js
+++ webapp2.js
@@ -2,17 +2,18 @@ const MongoClient = require('mongodb').MongoClient;
 
 const mongo_url = "mongodb+srv://jboyd1792:bf7CRKSwNjKyT8c5@cluster0.oqxp9cg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// async function main() {
-//   try {
-//     const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-//     console.log("Connected to MongoDB");
+async function main() {
+  try {
+    const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Connected to MongoDB");
 
-//     const db = client.db("dbhw");
-//     const collection = db.collection('places');
+    const db = client.db("dbhw");
+    const collection = db.collection('places');
 
     var http = require('http');
     var url = require('url');
     var port = process.env.PORT || 3000;
+      
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         var path = url.parse(req.url).pathname;
@@ -32,10 +33,9 @@ const mongo_url = "mongodb+srv://jboyd1792:bf7CRKSwNjKyT8c5@cluster0.oqxp9cg.mon
         }
         res.end();
     }).listen(port);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-// main();
+main();
