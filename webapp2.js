bd55cb6f05cb3ebf7481c9a6e1117c62440a62cd
10jeb <57235747+10jeb@users.noreply.github.com>
2024-04-25 14:09:43 -0500

    Update webapp2.js

--- webapp2.js
+++ webapp2.js
@@ -1,22 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const mongo_url = "mongodb+srv://jboyd1792:bf7CRKSwNjKyT8c5@cluster0.oqxp9cg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-async function main() {
-  try {
-    const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log("Connected to MongoDB");
-
-    const db = client.db("dbhw");
-    const collection = db.collection('places');
-
-
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-main();
-
 var http = require('http');
 var url = require('url');
 var port = process.env.PORT || 3000;
@@ -31,6 +15,9 @@ http.createServer(function (req, res) {
                 "</form>";
         res.write(form);
     } else if (path == "/process"){
+        const client = await MongoClient.connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db("dbhw");
+        const collection = db.collection('places');
         if (/^\d/.test(url.parse(req.url, true).query.data)) {
             res.write("You entered a zipcode: " + url.parse(req.url, true).query.data);
         } else {
